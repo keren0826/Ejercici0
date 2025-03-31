72bc8b05cdde0e2ea8064825113b285f460a5a62
keren0826 <kerenbecerra306@gmail.com>
2025-03-31 08:20:22 -0600

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,12 @@
+function Calcula_promedio()
+{
+var n1=parseFloat(document.getElementById("numero1").value);
+var n2=parseFloat(document.getElementById("numero2").value);
+var n3=parseFloat(document.getElementById("numero3").value);
+
+var p=(n1+n2+n3)/3;
+
+
+
+document.getElementsByName("total")[0].value = p;
+}
